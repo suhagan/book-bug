ee34cafaff5ef3fcf90805600b3e94b78830d2ff
Suhagan Mostahid <55191411+suhagan@users.noreply.github.com>
2025-12-03 09:28:34 +0100

    "data-testid" introduced in header to complete the test.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,28 +5,33 @@ export const Header: React.FC = () => {
   const { totalItems } = useCart();
 
   return (
-    <header className="header">
+    <header className="header" data-testid="header">
       <div className="signup-link">
         New here?{" "}
-        <Link 
-          to="/register/step-1" 
+        <Link
+          to="/register/step-1"
           className="signup-anchor"
+          data-testid="signup-link"
         >
           Sign Up
         </Link>
       </div>
       <div className="header-inner">
-        <Link to="/" className="logo">
+        <Link to="/" className="logo" data-testid="logo">
           BookBug
         </Link>
-        <nav className="nav">
-          <NavLink to="/" className="nav-link">
+        <nav className="nav" data-testid="main-nav">
+          <NavLink to="/" className="nav-link" data-testid="nav-home">
             Home
           </NavLink>
-          <NavLink to="/cart" className="nav-link">
+          <NavLink to="/cart" className="nav-link" data-testid="nav-cart">
             Cart ({totalItems})
           </NavLink>
-          <NavLink to="/checkout" className="nav-link">
+          <NavLink
+            to="/checkout"
+            className="nav-link"
+            data-testid="nav-checkout"
+          >
             Checkout
           </NavLink>
         </nav>
